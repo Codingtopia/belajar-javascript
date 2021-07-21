918fa934f89d7e78d224c4f4a521b8b2983f40bb
Aclaputra <muhamadacla22@gmail.com>
2021-07-22 01:21:33 +0700

    added latihan-js-5/

---
+++ latihan-js-5/script.js
@@ -0,0 +1,13 @@
+// document.write("Halo Javascript Variabel");
+
+let orang = true;
+console.log(orang);
+
+orang = false;
+console.log(orang);
+
+function coba() {
+    console.log(orang);
+}
+
+coba();
